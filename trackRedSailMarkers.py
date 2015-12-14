4cf1376ea4b6469aefd678c75b0fcf5baaaecf15
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-12-14 02:40:28 -0500

    Good success with tracking and color thresholding. not perfect.

--- trackRedSailMarkers.py
+++ trackRedSailMarkers.py
@@ -5,28 +5,35 @@ filename = 'Transport TestFlutter0.mp4'
 
 
 def make_image_mask(img):
-#    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-#    ret,thresh = cv2.threshold(img,40,255,cv2.THRESH_BINARY)
-#         define range of blue color in HSV
-    lower_blue = np.array([60,30,55])
-    upper_blue = np.array([200,50,80])
-
+    # B G R
+    lower_blue = np.array([0,0,120])# np.array([60,30,55])
+    upper_blue = np.array([70,70,200])
+    
     # Threshold the HSV image to get only blue colors
     thresh = cv2.inRange(img, lower_blue, upper_blue)
+    
     cv2.imshow('mask', thresh)
     return thresh
 
-def get_circle(frame):    
+def get_circle(frame): 
+       
     side_mask = make_image_mask(frame)
     contours, hierarchy = cv2.findContours(side_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
-###    cnt = contours[0]
-###    (x,y),radius = cv2.minEnclosingCircle(cnt)
-###    side_center = (int(x),int(y))
-###    side_radius = int(radius)
-    #print 'center: ', side_center, 'radius: ', side_radius, ' found from side camera'
+    print len(contours)
+    cnt = contours[0]
+    (x,y),radius = cv2.minEnclosingCircle(cnt)
+    center = (int(x),int(y))
+    radius = int(radius)
+    side_mask = cv2.cvtColor(side_mask, cv2.COLOR_GRAY2RGB)
+    #cv2.circle(side_mask, (447,63), 63, (0,0,255), -1)
+    cv2.circle(side_mask, center, radius, np.array([0,0,255]), 10)
+    cv2.imshow('mask', side_mask)
+    print 'center: ', center, 'radius: ', radius, ' found from side camera'
     
 ###    return side_center, radius
+
+    return 1, 2
 
 
 
@@ -35,35 +42,13 @@ cap = cv2.VideoCapture(filename)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    center, radius = get_circle(frame)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',gray)
+    #cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
-
-###for i in range(start_val,end_val):
-###    # Capture frame-by-frame
-###    #frame = cv2.imread('5-11_data_collection/test{}/test{}image{}.jpg'.format(str(testNum), str(testNum), str(i)))
-###    
-###    
-
-###    # Our operations on the frame come here
-###    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-######    center, radius = get_circle(frame)
-###    get_circle(frame)
-######    radius = int(radius)
-######    print center , radius
-######    img = cv2.circle(frame,center,radius,(0,255,0),2)
-###    # Display the resulting frame
-###    cv2.imshow('frame',frame)
-###    time.sleep(.1)
-
-###    if cv2.waitKey(1) & 0xFF == ord('q'):
-###        print 'hi'
-
-#### When everything done, release the capture
 cv2.destroyAllWindows()
