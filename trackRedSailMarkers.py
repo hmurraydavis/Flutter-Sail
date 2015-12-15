18d4fe72718d5a66c0fc0fce7c116288c3e17fbc
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-12-15 01:41:55 -0500

    Red threshold good enough

--- trackRedSailMarkers.py
+++ trackRedSailMarkers.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import pprint
+import time
 
 filename = 'Data1/t2.mp4'
 
@@ -13,21 +14,16 @@ centerArrayX = []; centerArrayY = []
 
 def make_image_mask(img):
     # B G R
-    lower_blue = np.array([0,0,120],  dtype=np.uint8)# np.array([60,30,55])
-    upper_blue = np.array([70,70,200],  dtype=np.uint8)
+    lower_blue = np.array([23,0,100],  dtype=np.uint8)# np.array([60,30,55])
+    upper_blue = np.array([100,80,255],  dtype=np.uint8)
     
     # Threshold the HSV image to get only blue colors
-    ##print 'hi 1'
     try:
-        ##print 'trying thresh'
         thresh = cv2.inRange(img, lower_blue, upper_blue)
-        ##print 'thresh successful: '
-        ##print thresh
         #cv2.imshow('mask', thresh)
         return thresh
     except:
         print "Fatal inRange error!"
-        cv2.imshow('mask', img)
         return ''
     
     
@@ -53,14 +49,20 @@ def get_circle(frame):
             side_mask = cv2.cvtColor(side_mask, cv2.COLOR_GRAY2RGB)
             
             cv2.circle(side_mask, center, radius, np.array([0,0,255]), 10)
-            cv2.imshow('mask', side_mask)
+            
             #print 'center: ', center, 'radius: ', radius, ' found from side camera'
             
 #            return center, radius
 
-#        else:
-#            numberErrors = numberErrors + 1
-#            print 'Current number of error frames is: ', numberErrors
+        else:
+            numberErrors = numberErrors + 1
+            print 'Current number of error frames is: ', numberErrors, ' Out of: ', frameNum
+            print 'percent error is: ', float(numberErrors)/frameNum
+            cv2.circle(side_mask, center, radius, np.array([0,255,0]), 10)
+            #cv2.imshow('mask', frame)#side_mask)
+            #cv2.imshow('mask', side_mask)
+            time.sleep(.2)
+        cv2.imshow('mask', side_mask)
 #            return 0, 0
 
 
@@ -70,7 +72,6 @@ cap = cv2.VideoCapture(filename)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    #center, radius = 
     get_circle(frame)
     centerP, radiusP = center, radius
     
@@ -81,15 +82,13 @@ while(cap.isOpened()):
         'In P block. centerP is: ', centerP
         centerArrayX.append(centerP[0])
         centerArrayY.append(centerP[1])
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     frameNum =frameNum + 1
     ##print 'Frame num is: ', frameNum
-    if frameNum == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):#frameNum > 273:
-        #pprint.pprint(centerArray)
+    if frameNum == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
         plt.plot(centerArrayX, label = 'X marker positon', color='#AA3C39', linewidth=6)
         plt.plot(centerArrayY, label = 'Y marker positon', color='#7A9E35', linewidth=6)
         plt.xlabel('Time', fontsize = 18)
