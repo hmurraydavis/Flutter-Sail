fd892a79eeb73a2732ebdd78951a19c893a6cbe1
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-12-16 23:20:52 -0500

    initial commit

---
+++ plotSavedData.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pprint
+import math
+import pickle
+import scipy.stats as stats
+import trackRedSailMarkers as tsm
+
+testNum = 4
+
+filename = 'Data1/t'+str(testNum)+'.mp4'
+
+
